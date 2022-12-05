87e49f6864350016eadc06a69fc5f16fd4af6e12
anikatahsin12 <bushratahsin653@gmail.com>
2022-12-05 21:02:50 +0600

    useEffect completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount1((count1) => count1 + 1);
+    }, 1000);
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount2((count2) => count2 + 1);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    setCalculation(() => count3 * 2);
+  }, [count3]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I've rendered {count1} times!</h1>
+      <h1>I've rendered {count2} times!</h1>
+      <p>Count: {count3}</p>
+      <button onClick={() => setCount3((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
     </div>
   );
 }
